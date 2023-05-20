cf9b7e159fc328f8a48ba4c7a621d749d7706934
abecerraguz <alejandro.becerra@publimetro.cl>
2023-05-20 05:10:34 -0400

    Se pasa la funcion getPoke de promesa a async Await

--- src/components/getpokemon.js
+++ src/components/getpokemon.js
@@ -3,18 +3,42 @@ import * as UI from './interfaz.js'
 import getPokeDetails from  './getPokeDetails.js';
 // const contentSpinnerLoading = document.querySelector('.contentSpinnerLoading')
 // console.log('XXXXX--->',contentSpinnerLoading)
-const getPokeMon = (offset='0')=>{
+const getPokeMon = async (offset='0')=>{
+
     UI.contentSpinnerLoading.style.display = 'flex'
-    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`)
-        .then( result =>{
-            result.json()
-                .then( respuesta =>{
-                    const {results} = respuesta
-                    getPokeDetails(results)
-                    UI.contentSpinnerLoading.style.display = 'none'
-                    // console.log('Salida de respuesta-->',results)
+
+    try {
+        UI.contentSpinnerLoading.style.display = 'flex';
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`)
+        console.log('Salida')
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json()
+        const { results } = data
+        getPokeDetails(results)
+        UI.contentSpinnerLoading.style.display = 'none';
+    } catch (error) {
+        //console.log(error)
+        // mensajeModal(`<p>${error}</p>`)
+        // $('#mensajeModal').modal('toggle');
+        // setTimeout(() => {
+        //     $('#mensajeModal').modal('toggle');
+        // }, 3000);
+    }
+
+    // fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`)
+    //     .then( result =>{
+    //         result.json()
+    //             .then( respuesta =>{
+    //                 const {results} = respuesta
+    //                 getPokeDetails(results)
+    //                 UI.contentSpinnerLoading.style.display = 'none'
+    //                 // console.log('Salida de respuesta-->',results)
                     
-                })
-        })
+    //             })
+    //     })
 }
 export default getPokeMon
